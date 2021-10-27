8cff166f9b25f58e1015e3ed29e418b62ffce619
Tomasz Klocek <tomek@klocek.info>
2021-10-27 15:10:50 +0200

    Initial Commit

---
+++ KursyNBP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KursyNBP
+//
+//  Created by Tomasz Klocek on 2021-10-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ KursyNBPUITests/KursyNBPUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  KursyNBPUITestsLaunchTests.swift
+//  KursyNBPUITests
+//
+//  Created by Tomasz Klocek on 2021-10-27.
+//
+
+import XCTest
+
+class KursyNBPUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
